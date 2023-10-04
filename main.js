26ca1d81201b3e17a1caf100935ec20ef836b2f6
Cocera <alcocera@gmail.com>
2023-10-04 15:10:20 +0200

    Exercise 2, foreach

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-// EJERCICIO 1
+// 1 EJERCICIO ARROW
 
 const saludar = () => "Hola";
 console.log(saludar());
@@ -15,4 +15,69 @@ const test2 = () => console.log("Función test 2 ejecutada.");
 const test1 = (callback) => callback();
 test1(test2);
 
-//EJERCICIO 2
+// 2 EJERCICIO FOREACH
+
+let gente = [
+    {
+      nombre: "Jamiro",
+      edad: 45,
+    },
+    {
+      nombre: "Juan",
+      edad: 35,
+    },
+    {
+      nombre: "Paco",
+      edad: 34,
+    },
+    {
+      nombre: "Pepe",
+      edad: 14,
+    },
+    {
+      nombre: "Pilar",
+      edad: 24,
+    },
+    {
+      nombre: "Laura",
+      edad: 24,
+    },
+    {
+      nombre: "Jenny",
+      edad: 10,
+    },
+  ];
+
+const older25 = [];
+gente.forEach(user => {
+    if(user.edad > 25) {
+        older25.push(user);
+    }
+});
+console.log(older25);
+
+const startsWithJ = [];
+gente.forEach (user => {
+    if (user.nombre.startsWith("J")) {
+    startsWithJ.push(user)
+    }
+});
+console.log(startsWithJ);
+
+    // OPTION 2
+    // const startsWithJ = [];
+    // gente.forEach (user => {
+    //     if (user.nombre[0] === 'J') {
+    //     startsWithJ.push(user)
+    //     }
+    // });
+    // console.log(startsWithJ);
+
+
+// 3 EJERCICIO MAP
+const mapOlder25 = gente.map() 
+
+
+
+
+
